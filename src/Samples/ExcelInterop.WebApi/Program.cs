cdccedcd5f1d5e012ce4d37ecb4b13667f34fb41
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-09-14 16:05:36 +1000

    initial add

---
+++ src/Samples/ExcelInterop.WebApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelInterop.WebApi
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running web-api sample..");
+
+            List<Todo> todos = GetTodos().Result;
+
+            Console.WriteLine("Finished! - "+todos.Count());
+            Console.Read();
+        }
+
+        private static async Task<List<Todo>> GetTodos()
+        {
+            var jsonTask = client.GetStreamAsync("https://jsonplaceholder.typicode.com/todos");
+ 
+            var serializer = new DataContractJsonSerializer(typeof(List<Todo>));
+            var todos = serializer.ReadObject(await jsonTask) as List<Todo>;
+            return todos;
+        }
+    }
+}
---
+++ src/Samples/ExcelInterop.WebApi/Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelInterop.WebApi
+{
+    public class Todo
+    {
+        [DataMember(Name = "userId")]
+        public int UserId { get; set; }
+
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "title")]
+        public string Tittle { get; set; }
+
+        [DataMember(Name = "completed")]
+        public bool Completed { get; set; }
+    }
+}
